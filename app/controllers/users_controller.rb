bd092db50fdfcaf8a5b35528fba9a7d5837e28f6
Zalman <moishkugel@gmail.com>
2023-03-28 13:57:45 -0400

    commit tree 8351154dce1cf5c003e0343a5365f2ba6188c834

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,27 @@
+class UsersController < ApplicationController
+
+  def create
+    user = User.create(user_params)
+    if user.valid?
+      render json: user, status: :created
+      session[:user_id] = user.id
+    else
+      render json: { errors: user.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  def show
+    if session[:user_id]
+      user = User.find(session[:user_id])
+      render json: user
+    else
+      render json: { error: "Unauthorized user" }, status: :unauthorized
+    end
+  end
+
+  private
+
+  def user_params
+    params.permit(:username, :password, :password_confirmation)
+  end
+end
